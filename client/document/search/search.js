8ecbec17a45c1079e40b8f6941b3ffa08ab6691c
Davi <davihugomp@gmail.com>
2021-03-15 21:51:23 -0300

    Changes on search and index

--- client/document/search/search.js
+++ client/document/search/search.js
@@ -6,16 +6,11 @@ $(function() {
     var docsTable = $("#docsTable");
 
     modalBtn.on("click", function(){
-        openModal(modalContainer, "components/modal/modal.html", "Editar", "document/manage/manage.html");
     });
 
     docsTable.on('click', 'tbody tr', function() {
-        /**
-        select_stock(symbol);
-        * 
-        */
         let current = $(this);
-        let symbol = current.find("td").eq(0).html();
-        console.log("Hi", symbol);
+        let name = current.find("td").eq(0).html();
+        openModal(modalContainer, "components/modal/modal.html", `${name}`, "document/create/createDocument.html");
     });
 });
